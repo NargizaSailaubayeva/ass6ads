4c00283447ea21f9e06ffba486542bc2fdf1a475
Nargiza <nargizok04s@gmail.com>
2023-05-31 15:18:27 +0500

    findpath

--- src/DijkstraSearch.java
+++ src/DijkstraSearch.java
@@ -68,8 +68,59 @@ public class DijkstraSearch<V> implements Search<V> {
 
         return distances;
     }
+    private List<V> buildPath(Map<Vertex<V>, Vertex<V>> parentMap, Vertex<V> destination) {
+        List<V> path = new ArrayList<>();
+        Vertex<V> currentVertex = destination;
+
+        while (currentVertex != null) {
+            path.add(0, currentVertex.getData());
+            currentVertex = parentMap.get(currentVertex);
+        }
+
+        return path;
+    }
     @Override
     public List<V> findPath(Vertex<V> source, Vertex<V> destination) {
-        return null;
+        Map<Vertex<V>, Double> distances = new HashMap<>();
+        Map<Vertex<V>, Vertex<V>> parentMap = new HashMap<>();
+        PriorityQueue<DijkstraNode<V>> priorityQueue = new PriorityQueue<>();
+
+        // Initialize distances to infinity for all vertices except the source vertex
+        for (Vertex<V> vertex : graph.getVertices()) {
+            if (vertex.equals(source)) {
+                distances.put(vertex, 0.0);
+            } else {
+                distances.put(vertex, Double.POSITIVE_INFINITY);
+            }
+        }
+
+        priorityQueue.offer(new DijkstraNode<>(source, 0.0));
+
+        while (!priorityQueue.isEmpty()) {
+            DijkstraNode<V> currentNode = priorityQueue.poll();
+            Vertex<V> currentVertex = currentNode.getVertex();
+            double currentDistance = currentNode.getDistance();
+
+            // Skip if the current distance is greater than the known distance
+            if (currentDistance > distances.get(currentVertex)) {
+                continue;
+            }
+
+            List<WeightedGraph<V>.Edge<V>> edges = graph.getEdge(currentVertex);
+            for (WeightedGraph<V>.Edge<V> edge : edges) {
+                Vertex<V> neighborVertex = edge.getDestination();
+                double edgeWeight = edge.getWeight();
+                double distanceThroughCurrent = currentDistance + edgeWeight;
+
+                // Update the distance if it is shorter than the current known distance
+                if (distanceThroughCurrent < distances.get(neighborVertex)) {
+                    distances.put(neighborVertex, distanceThroughCurrent);
+                    parentMap.put(neighborVertex, currentVertex);
+                    priorityQueue.offer(new DijkstraNode<>(neighborVertex, distanceThroughCurrent));
+                }
+            }
+        }
+
+        return buildPath(parentMap, destination);
     }
 }
