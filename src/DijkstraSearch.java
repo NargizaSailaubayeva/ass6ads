ccc2c3eb28d29357e92033a356a049831d5efb62
Nargiza <nargizok04s@gmail.com>
2023-05-31 14:05:03 +0500

    DijkstraSearch private class

--- src/DijkstraSearch.java
+++ src/DijkstraSearch.java
@@ -5,6 +5,28 @@ public class DijkstraSearch<V> implements Search<V> {
     public DijkstraSearch(WeightedGraph<V> graph) {
         this.graph = graph;
     }
+    private static class DijkstraNode<V> implements Comparable<DijkstraNode<V>> {
+        private Vertex<V> vertex;
+        private double distance;
+
+        public DijkstraNode(Vertex<V> vertex, double distance) {
+            this.vertex = vertex;
+            this.distance = distance;
+        }
+
+        public Vertex<V> getVertex() {
+            return vertex;
+        }
+
+        public double getDistance() {
+            return distance;
+        }
+
+        @Override
+        public int compareTo(DijkstraNode<V> other) {
+            return Double.compare(distance, other.distance);
+        }
+    }
     @Override
     public List<V> findPath(Vertex<V> source, Vertex<V> destination) {
         return null;
