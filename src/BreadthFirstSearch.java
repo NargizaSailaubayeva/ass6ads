eb374f01a0ca5ba37c6790cace0394282ca24ee0
Nargiza <nargizok04s@gmail.com>
2023-05-31 13:16:52 +0500

    breadthFirstSearch

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,12 +1,35 @@
-import java.util.List;
+import java.util.*;
 
 public class BreadthFirstSearch<V> implements Search<V> {
     private WeightedGraph<V> graph;
     public BreadthFirstSearch(WeightedGraph<V> graph) {
         this.graph = graph;
     }
+    public List<Vertex<V>> breadthFirstSearch(Vertex<V> startVertex) {
+        List<Vertex<V>> visited = new ArrayList<>();
+        Queue<Vertex<V>> queue = new LinkedList<>();
+
+        visited.add(startVertex);
+        queue.add(startVertex);
+
+        while (!queue.isEmpty()) {
+            Vertex<V> currentVertex = queue.poll();
+            List<WeightedGraph<V>.Edge<V>> edges = graph.getEdge(currentVertex);
+
+            for (WeightedGraph<V>.Edge<V> edge : edges) {
+                Vertex<V> neighborVertex = edge.getDestination();
+                if (!visited.contains(neighborVertex)) {
+                    visited.add(neighborVertex);
+                    queue.add(neighborVertex);
+                }
+            }
+        }
+
+        return visited;
+    }
     @Override
     public List<V> findPath(Vertex<V> source, Vertex<V> destination) {
         return null;
     }
+
 }
